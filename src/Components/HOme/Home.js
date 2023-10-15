ee3705251013ac860bae5407e34f475d48cfe17f
Sumaiya-topu <topusumaiya@gmail.com>
2023-10-15 16:30:44 +0600

    Error: gradio dependency

--- src/Components/HOme/Home.js
+++ src/Components/HOme/Home.js
@@ -2,8 +2,7 @@ import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import "./Home.css";
 import Table from "./Table";
-import AudioPlayer from "./AudioPlayer";
-// import { client } from "@gradio/client";
+import { client } from "@gradio/client";
 import axios from "axios";
 const Home = () => {
   const [inputData, setInputData] = useState({ promptText: "" });
@@ -14,32 +13,19 @@ const Home = () => {
   } = useForm();
 
   const handlePromptSubmit = async (data) => {
-    // const app = await client("https://6969c6492bec4f017d.gradio.live/"); // Replace with your Gradio interface URL
-    // const result = await app.predict(0, [
-    //   data.description,
-    //   data.top_k,
-    //   data.top_p,
-    //   data.temperature,
-    //   data.duration,
-    //   data.cfg_coef,
-    // ]);
+    const app = await client("https://3fdf06e9e735315c0f.gradio.live"); // Replace with your Gradio interface URL
+    const result = await app.predict(0, [
+      data.description,
+      data.top_k,
+      data.top_p,
+      data.temperature,
+      data.duration,
+      data.cfg_coef,
+    ]);
 
-    // Send the result to JSON Server
-    // const response = await fetch("http://localhost:3000/results", {
-    //   method: "POST",
-    //   headers: {
-    //     "Content-Type": "application/json",
-    //   },
-    //   body: JSON.stringify({ audio_url: result.data }),
-    // });
-
-    // if (response.ok) {
-    // Handle a successful response (e.g., show a success message)
-    // } else {
-    // Handle errors
-    // }
+    const newData = { ...data, audio: result.data };
     axios
-      .post("http://localhost:8000/prompts", data)
+      .post("http://localhost:8000/prompts", newData)
       .then((res) => {
         alert("Prompt added successfully!");
       })
@@ -48,35 +34,107 @@ const Home = () => {
   };
   return (
     <div className="">
-      <div className="home h-screen pt-[200px]">
-        <div className="flex justify-center">
+      <div className="home h-screen pt-[100px]">
+        <div className="flex justify-end pr-10">
           {" "}
-          <h1 className="text-white">
-            Welcome to the Future of Music: AI-Driven Melodies
-          </h1>
-        </div>
-        <form
-          onSubmit={handleSubmit(handlePromptSubmit)}
-          className=" w-1/2 mx-auto"
-        >
-          <input
-            onChange={(e) =>
-              setInputData({ ...inputData, promptText: e.target.value })
-            }
-            {...register("promptText")}
-            type="text"
-            className="w-full p-2 border h-20 border-gray-300 rounded-md mb-4 bg-transparent"
-          />
+          <div className="">
+            <h1 className="text-white">
+              Welcome to <br />
+              <span className="text-4xl text-[#ED7014]  font-semibold">
+                {" "}
+                The Future of Music
+              </span>{" "}
+              <br />{" "}
+              <span className="text-2xl text-white"> AI-Driven Melodies</span>
+            </h1>
+            <form onSubmit={handleSubmit(handlePromptSubmit)} className="mt-5">
+              <input
+                onChange={(e) =>
+                  setInputData({ ...inputData, promptText: e.target.value })
+                }
+                {...register("promptText")}
+                type="text"
+                placeholder="Low tempo with synth music"
+                className="w-full p-2 border h-20 text-white border-gray-300 rounded-md mb-4 bg-transparent"
+              />
 
-          {errors.age && <p>Please enter number for age.</p>}
-          <div className="flex justify-center">
-            <input
-              type="submit"
-              className="text-white bg-[#ED7014] rounded-lg block px-10 py-2"
-              value="Generate Tune"
-            />
+              {errors.age && <p>Please enter number for age.</p>}
+              <div className="flex gap-1">
+                <div>
+                  <label htmlFor="" className="text-white">
+                    Top_k
+                  </label>
+                  <input
+                    onChange={(e) =>
+                      setInputData({ ...inputData, promptText: e.target.value })
+                    }
+                    {...register("top-k")}
+                    type="text"
+                    className="w-full p-2 border text-white border-gray-300 rounded-md mb-4 bg-transparent"
+                  />
+                </div>
+
+                {errors.age && <p>Please enter number for age.</p>}
+                <div>
+                  {" "}
+                  <label htmlFor="" className="text-white">
+                    Top_p
+                  </label>
+                  <input
+                    onChange={(e) =>
+                      setInputData({ ...inputData, promptText: e.target.value })
+                    }
+                    {...register("top-p")}
+                    type="text"
+                    className="w-full p-2 border text-white border-gray-300 rounded-md mb-4 bg-transparent"
+                  />
+                  {errors.age && <p>Please enter number for age.</p>}
+                </div>
+              </div>
+              <div className="flex gap-1">
+                <div>
+                  <label htmlFor="" className="text-white">
+                    Duration
+                  </label>
+                  <input
+                    onChange={(e) =>
+                      setInputData({ ...inputData, promptText: e.target.value })
+                    }
+                    {...register("duration")}
+                    type="text"
+                    className="w-full p-2 border text-white border-gray-300 rounded-md mb-4 bg-transparent"
+                  />
+                </div>
+
+                {errors.age && <p>Please enter number for age.</p>}
+                <div>
+                  {" "}
+                  <label htmlFor="" className="text-white">
+                    Cfg-co
+                  </label>
+                  <input
+                    onChange={(e) =>
+                      setInputData({ ...inputData, promptText: e.target.value })
+                    }
+                    {...register("cfgco")}
+                    type="text"
+                    className="w-full p-2 border text-white border-gray-300 rounded-md mb-4 bg-transparent"
+                  />
+                  {errors.age && <p>Please enter number for age.</p>}
+                </div>
+              </div>
+
+              {errors.age && <p>Please enter number for age.</p>}
+              <div className="flex justify-center">
+                <input
+                  type="submit"
+                  className="text-white bg-[#ED7014] rounded-lg block px-10 py-2"
+                  value="Generate Tune"
+                />
+              </div>
+            </form>
           </div>
-        </form>
+        </div>
       </div>
       <div className="mt-20 w-3/4 mx-auto">
         <h1 className="text-[#ED7014] text-2xl mb-5 font-semibold ">
